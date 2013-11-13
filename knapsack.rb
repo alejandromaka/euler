8a76374f973d11d013697d7ec552bf77319434f3
Alejandro <alejandro@alejandro.local>
2013-11-13 14:24:04 -0500

    first commit

---
+++ knapsack.rb
@@ -0,0 +1,18 @@
+
+class Knapsack
+	def knapsack(index, size, weight, value)
+
+	end
+
+	def start
+		puts "enter all weights: "
+		gets weight
+		puts "now enter all values (respectively): "
+		gets values
+		puts weight
+		puts values
+	end
+end
+
+object = Knapsack.new
+object.start
---
+++ readme.md
@@ -0,0 +1,4 @@
+my knapsack ruby solution
+=========================
+
+After seeing a few solutions, I decided to make a cleaner version of them on ruby.
