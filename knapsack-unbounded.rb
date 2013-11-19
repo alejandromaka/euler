48b24542c05e2799a0b32dcae4ce662345ad5a85
Alejandro <alejandro@alejandro.local>
2013-11-19 12:10:47 -0500

    finished implementing the unbounded solution. now on to the 0,1 knapsack problem

---
+++ knapsack-unbounded.rb
@@ -0,0 +1,43 @@
+
+class Knapsack
+
+	def knapsack(size, weight, value)
+		#return if knapsack's size is 0 or when there's not enough space to keep adding items
+		return 0 if size == 0 || weight.min > size
+
+		#store all the solutions in a temp array
+		allSolutions = []
+		for i in 0..weight.size-1
+			if weight[i] <= size
+				# iterate through all options and add the previous result
+				allSolutions[i] = knapsack(size-weight[i], weight, value) + value[i]
+			else
+				# assign the variable 0 so it can be added
+				allSolutions[i] = 0
+			end
+		end
+		return allSolutions.max
+	end
+
+	def start
+		#ask for info
+		begin 
+			puts "enter the size of the knapsack: "
+			size = gets.to_i
+			puts "enter all weights separated by a space: "
+			weight = gets.split(" ").map(&:to_i)
+			puts "now enter all values (respectively) separated by a space: "
+			value = gets.split(" ").map(&:to_i)
+		end while	((weight.size != value.size) || size <= 0)
+
+		# in case im lazy and want to test
+		# size = 10
+		# weight = [5,4,6,3]
+		# value = [10,40,30,50]
+		puts knapsack(size, weight, value)
+	end
+
+end
+
+object = Knapsack.new
+object.start
--- readme.md
+++ readme.md
@@ -1,4 +1,5 @@
-my knapsack ruby solution
-=========================
-
-After seeing a few solutions, I decided to make a cleaner version of them on ruby.
+my knapsack ruby solutions
+==
+<br/><b>unbounded:</b><br/>
+after seeing a few solutions online, i decided to make a cleaner version of them on ruby. this is an unbounded solution, which means that the items are theoretically limitless (infinite). <br/>
+<br/><b>other:</b><br/>
